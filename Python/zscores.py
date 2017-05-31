3ac3ed756ab6efeddf16bf3d42a0aa31f845bd2d
Scott Josephson <gedman4b@yahoo.com>
2017-05-31 01:53:17 -0400

    Update zscores.py

--- Python/zscores.py
+++ Python/zscores.py
@@ -1,10 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-##import plotly.plotly as py
-##import plotly.graph_objs as go
-##import plotly
-##import cufflinks as cf
 import seaborn as sns
 
 df = pd.read_csv('EXSC-01-14RD CLIENT DATA TABLE.csv')
@@ -12,16 +8,5 @@ df = pd.read_csv('EXSC-01-14RD CLIENT DATA TABLE.csv')
 # zscores = sns.load_dataset("EXSC-01-14RD CLIENT DATA TABLE.csv")
 
 zc = df[['EP-1','EP-2','EP-3','EP-4','EP-5','EP-6','EP-7','EP-8','EP-9']]
-##trace = go.Heatmap(z=zc)
-##data=[trace]
-##py.iplot(data, filename='zscores')
 
-##plotly.offline.plot({
-##	"data": trace
-##	})
-##plotly.offline.plot({
-##	"data": data
-##	})
 sns.heatmap(zc)
-ax = sns.heatmap(zc)
-plt.plot(ax)
